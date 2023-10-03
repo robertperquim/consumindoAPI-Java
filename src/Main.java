9aaf60f9e395041d7a2ca7e6ee968f011894b3aa
robertperquim <perquimrobert@gmail.com>
2023-10-02 21:40:29 -0300

    Primeira comunicacao com o webService -> precisa de melhorias

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=b38e6553"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
 }
